fe9aa11c9001d28319005ed72c560c949c85662f
Stuart Walker <stuart.walker@desy.de>
2024-05-27 17:55:37 +0200

    sbunchpanel mypy fixes

--- esme/gui/widgets/sbunchpanel.py
+++ esme/gui/widgets/sbunchpanel.py
@@ -1,15 +1,14 @@
 import logging
-import time
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtCore import pyqtSignal
 
-from esme.gui.ui.special_bunch_panel import Ui_special_bunch_panel
 from esme.control.pattern import get_beam_regions, get_bunch_pattern
-from .common import get_machine_manager_factory
 from esme.core import DiagnosticRegion
+from esme.gui.ui.special_bunch_panel import Ui_special_bunch_panel
+
+from .common import get_machine_manager_factory
 
 LOG = logging.getLogger(__name__)
 
@@ -21,19 +20,36 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
     # as a result of something from outside of this GUI.
     MAIN_TIMER_PERIOD_MS = 2500
     IS_FIRING_TIMER = 200
+
     def __init__(self, parent=None):
         super().__init__(parent=parent)
 
-        self.i1dbmanager = get_machine_manager_factory().make_diagnostic_bunches_manager(DiagnosticRegion.I1)
-        self.b2dbmanager = get_machine_manager_factory().make_diagnostic_bunches_manager(DiagnosticRegion.B2)
-        self.dbunch_manager = self.i1dbmanager # Set sbm choice to be for I1 diagnostics
+        self.i1dbmanager = (
+            get_machine_manager_factory().make_diagnostic_bunches_manager(
+                DiagnosticRegion.I1
+            )
+        )
+        self.b2dbmanager = (
+            get_machine_manager_factory().make_diagnostic_bunches_manager(
+                DiagnosticRegion.B2
+            )
+        )
+        self.dbunch_manager = (
+            self.i1dbmanager
+        )  # Set sbm choice to be for I1 diagnostics
 
         self.ui = Ui_special_bunch_panel()
         self.ui.setupUi(self)
 
-        self.ibfb_warning_dialogue = IBFBWarningDialogue(self.dbunch_manager.sbunches, parent=self)
-        self.ibfb_warning_dialogue.fire_signal.connect(self.unsafe_diagnostic_bunch_start)
-        self.ibfb_warning_dialogue.disable_ibfb_aff_signal.connect(lambda: self.dbunch_manager.sbunches.set_ibfb_state(on=False))
+        self.ibfb_warning_dialogue = IBFBWarningDialogue(
+            self.dbunch_manager.sbunches, parent=self
+        )
+        self.ibfb_warning_dialogue.fire_signal.connect(
+            self.unsafe_diagnostic_bunch_start
+        )
+        self.ibfb_warning_dialogue.disable_ibfb_aff_signal.connect(
+            lambda: self.dbunch_manager.sbunches.set_ibfb_state(on=False)
+        )
 
         self.connect_buttons()
 
@@ -51,7 +67,9 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
         firing and update the UI (enable/disable elements only).
 
         """
-        self.set_bunch_control_enabled(self.dbunch_manager.sbunches.is_diag_bunch_firing())
+        self.set_bunch_control_enabled(
+            self.dbunch_manager.sbunches.is_diag_bunch_firing()
+        )
 
     def update_ui(self) -> None:
         """Read values from the DOOCs server and update the daughter widget states here."""
@@ -61,10 +79,14 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
         self.ui.npulses_spinbox.setValue(self.dbunch_manager.sbunches.get_npulses())
         self.ui.ibfb_checkbox.setCheckState(self.get_ibfb_checkstate())
         self.ui.use_tds_checkbox.setChecked(self.dbunch_manager.sbunches.get_use_tds())
-        self.ui.use_fast_kickers_checkbox.setChecked(self.dbunch_manager.sbunches.would_use_kickers())
+        self.ui.use_fast_kickers_checkbox.setChecked(
+            self.dbunch_manager.sbunches.would_use_kickers()
+        )
 
     def _update_beam_region_and_bunch_ui(self) -> None:
-        self.ui.beamregion_spinbox.setValue(self.dbunch_manager.sbunches.get_beam_region() + 1)
+        self.ui.beamregion_spinbox.setValue(
+            self.dbunch_manager.sbunches.get_beam_region() + 1
+        )
         self.ui.bunch_spinbox.setValue(self.dbunch_manager.sbunches.get_bunch_number())
 
     def get_ibfb_checkstate(self) -> Qt.CheckState:
@@ -75,12 +97,6 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
         if xon ^ yon:
             return Qt.PartiallyChecked
         return Qt.Unchecked
-
-    def check_fast_kickers_state(self) -> None:
-        print(self.dbunch_manager.sbunches.would_use_kickers())
-        if self.dbunch_manager.sbunches.would_use_kickers():
-            return True
-        return False
 
     def set_bunch_control_enabled(self, enabled: bool) -> None:
         """Enable or disable UI elements based for modification,
@@ -99,12 +115,24 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
     def connect_buttons(self) -> None:
         """Just called during __init__ for where UI button callbacks are set."""
         self.ui.use_fast_kickers_checkbox.stateChanged.connect(self.toggle_kickers)
-        self.ui.use_tds_checkbox.stateChanged.connect(self.dbunch_manager.sbunches.set_use_tds)
-        self.ui.beamregion_spinbox.valueChanged.connect(lambda n: self.dbunch_manager.sbunches.set_beam_region(n - 1))
-        self.ui.bunch_spinbox.valueChanged.connect(self.dbunch_manager.sbunches.set_bunch_number)
-        self.ui.npulses_spinbox.valueChanged.connect(self.dbunch_manager.sbunches.set_npulses)
-        self.ui.go_to_last_laserpulse_pushbutton.clicked.connect(self.goto_last_bunch_in_machine)
-        self.ui.go_to_last_bunch_in_br_pushbutton.clicked.connect(self.goto_last_bunch_in_beam_region)
+        self.ui.use_tds_checkbox.stateChanged.connect(
+            self.dbunch_manager.sbunches.set_use_tds
+        )
+        self.ui.beamregion_spinbox.valueChanged.connect(
+            lambda n: self.dbunch_manager.sbunches.set_beam_region(n - 1)
+        )
+        self.ui.bunch_spinbox.valueChanged.connect(
+            self.dbunch_manager.sbunches.set_bunch_number
+        )
+        self.ui.npulses_spinbox.valueChanged.connect(
+            self.dbunch_manager.sbunches.set_npulses
+        )
+        self.ui.go_to_last_laserpulse_pushbutton.clicked.connect(
+            self.goto_last_bunch_in_machine
+        )
+        self.ui.go_to_last_bunch_in_br_pushbutton.clicked.connect(
+            self.goto_last_bunch_in_beam_region
+        )
         self.ui.ibfb_checkbox.stateChanged.connect(self.set_ibfb_state)
         self.ui.start_button.clicked.connect(self.safe_diagnostic_bunch_start)
         self.ui.stop_button.clicked.connect(self.stop_diagnostic_bunch)
@@ -116,12 +144,12 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
 
     def safe_diagnostic_bunch_start(self) -> None:
         """The diagnostic bunch should not be fired whilst the IBFB LFF is activated, otherwise the IBFB will
-        try and counter the impact """
-        if self.sbunches.is_either_ibfb_on():
+        try and counter the impact"""
+        if self.dbunch_manager.sbunches.is_either_ibfb_on():
             self.ibfb_warning_dialogue.show()
         self.dbunch_manager.sbunches.start_diagnostic_bunch()
 
-    def unsafe_diagnostic_bunch_start():
+    def unsafe_diagnostic_bunch_start(self):
         self.dbunch_manager.sbunches.start_diagnostic_bunch()
 
     def stop_diagnostic_bunch(self) -> None:
@@ -140,27 +168,6 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
     def set_kickers_for_screen(self, screen_name: str) -> None:
         self.dbunch_manager.set_kickers_for_screen(screen_name)
 
-    def set_use_fast_kickers(self, kicker_name: str) -> None:
-        """Set whether the fast kicker(s) should fire when the
-        diagnostic bunch is fired.  To do this"""
-        # Get the current kicker setpoint, for some reason...
-        ksp = self.kicker_setpoint
-        if self.ui.use_fast_kickers_checkbox.isChecked() and self.kicker_setpoint is not None:
-            if ksp is None:
-                return
-            kicker_name = ksp[0].name
-            # LOG.info(f"Enabling fast kickers for {self.screen_name}: kicker: {kicker_name}")
-            # Just use first kicker name and assume that all are then
-            # set (i.e they have the same kicker numbers---they should
-            # be configured as such on the doocs server...).
-            self.dbunch_manager.sbunches.set_kicker_name(kicker_name)
-            # Power on kickers if they are to be used.
-            if not self.dbunch_manager.sbunches.would_use_kickers():
-                self.dbunch_manager.sbunches.power_on_kickers()
-        else:
-            # Set kickers not to be used in the SBM.
-            self.dbunch_manager.sbunches.dont_use_kickers()
-
     def goto_last_bunch_in_machine(self) -> None:
         beam_regions = get_beam_regions(get_bunch_pattern())
         last_beam_region = beam_regions[-1]
@@ -168,11 +175,13 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
         beam_region_number = last_beam_region.idn
         diagnostic_bunch_number = nbunches + 1
         # assert last_beam_region > 0
-        LOG.info(f"Found last bunch in machine: BR = {beam_region_number}, last normal bunch no. = {nbunches}, diagnostic bunch no. = {diagnostic_bunch_number}")
+        LOG.info(
+            f"Found last bunch in machine: BR = {beam_region_number}, last normal bunch no. = {nbunches}, diagnostic bunch no. = {diagnostic_bunch_number}"
+        )
         self.dbunch_manager.sbunches.set_beam_region(beam_region_number - 1)
         self.dbunch_manager.sbunches.set_bunch_number(diagnostic_bunch_number)
 
-        self.ui.beamregion_spinbox.setValue(beam_region_number_number)
+        self.ui.beamregion_spinbox.setValue(beam_region_number)
         self._update_beam_region_and_bunch_ui()
 
     def goto_last_bunch_in_beam_region(self) -> None:
@@ -183,8 +192,10 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
         try:
             br = beam_regions[selected_beam_region]
         except IndexError:
-            LOG.info(f"User tried to select last bunch of nonexistent beam region: {selected_beam_region}.")
-            box = QMessageBox(self) #, "Invalid Beam Region",
+            LOG.info(
+                f"User tried to select last bunch of nonexistent beam region: {selected_beam_region}."
+            )
+            box = QMessageBox(self)  # , "Invalid Beam Region",
             box.setText(f"Beam Region {selected_beam_region+1} does not exist.")
             box.exec()
             return
@@ -193,15 +204,16 @@ class SpecialBunchMidLayerPanel(QtWidgets.QWidget):
         self._update_beam_region_and_bunch_ui()
 
 
-
 class IBFBWarningDialogue(QMessageBox):
     SHORT_TEXT = """IBFB Adaptive FF is still on.  Disable and start firing diagnostic bunches?"""
-    LONG_TEXT = ("The IBFB Adaptive Feed Forward has"
-                 " not been disabled but you are trying to fire a diagnostic bunch."
-                 " It is generally advisable to disable IBFB Adaptive FF before firing a diagnostic"
-                 " bunch, otherwise the IBFB will try and counteract the TDS and"
-                 " fast kicker kicks, which will cause problems all along the bunch train"
-                 " and result in an otherwise parasitic measurement becoming invasive.")
+    LONG_TEXT = (
+        "The IBFB Adaptive Feed Forward has"
+        " not been disabled but you are trying to fire a diagnostic bunch."
+        " It is generally advisable to disable IBFB Adaptive FF before firing a diagnostic"
+        " bunch, otherwise the IBFB will try and counteract the TDS and"
+        " fast kicker kicks, which will cause problems all along the bunch train"
+        " and result in an otherwise parasitic measurement becoming invasive."
+    )
     fire_signal = pyqtSignal()
     disable_ibfb_aff_signal = pyqtSignal()
 
@@ -211,7 +223,7 @@ class IBFBWarningDialogue(QMessageBox):
         self.sbinterfaces = sbinterface
 
         self.setIcon(QMessageBox.Warning)
-        self.setWindowTitle('Warning')
+        self.setWindowTitle("Warning")
         self.setText(self.SHORT_TEXT)
         self.setDetailedText(self.LONG_TEXT)
 
@@ -223,10 +235,10 @@ class IBFBWarningDialogue(QMessageBox):
         # Connect buttons to functions
         self.buttonClicked.connect(self.on_button_clicked)
 
-    def on_button_clicked(self, button: QAbstractButton) -> None:
+    def on_button_clicked(self, button: QtWidgets.QAbstractButton) -> None:
         if button == self.disable_ibfb_lff_button:
             self.disable_ibfb_lff_button.emit()
-            self.enable_ibfb_aff_signal.emit()
+            self.fire_signal.emit()
         elif button == self.ignore_and_go_button:
             self.fire_signal.emit()
         self.hide()
